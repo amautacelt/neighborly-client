6bee586f53d48821d04ae2a419f513fd8934c430
Carlos E. La Torre <amautacelt@gmail.com>
2021-06-02 11:22:11 -0400

    added edit and delete button

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -5,9 +5,16 @@ const TaskCard = props => {
         <div className="column">
             <div className="card">
                 <h3 className="header">{props.task.name}</h3>
-                <p>
-                    {props.task.category}
-                </p>
+                <p>Category: {props.task.category}</p>
+                <p>Description: {props.task.description}</p>
+                <p>Duration: {props.task.duration}</p>
+                <button className="edit-button">
+                    edit
+                </button>
+                <button className="delete-button">
+                    delete
+                </button>
+
             </div>
         </div>
     )
