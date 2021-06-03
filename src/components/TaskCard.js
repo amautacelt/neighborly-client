3646ac2bd84be9c6d920c75d1035c4000c6079d0
Carlos E. La Torre <amautacelt@gmail.com>
2021-06-03 17:52:14 -0400

    css

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -17,28 +17,36 @@ class TaskCard extends Component {
     render() {
         // console.log(this.props.task)
         return (
-            <div className="column">
-                <div className="card">
-                    <h3 className="header">{this.props.task.name}</h3>
-                    <p>Category: {this.props.task.category}</p>
-                    <p>Description: {this.props.task.description}</p>
-                    <p>Duration: {this.props.task.duration}</p>
-                    <p>Has Volunteer: {this.props.task.has_volunteer ? "True" : "False"}</p>
-                    <button onClick={this.showEditForm}> {this.state.buttonEditLabel}</button>
-                        {
-                            this.state.showEditForm 
-                            ? <EditTaskForm task={this.props.task} 
-                                            options={this.props.categories} 
-                                            updateTask={this.props.updateTask}
-                                /> 
-                            : null
-                        }
-                    {/* <button><Link to='/tasks/edit'>Edit Task</Link></button>     */}
-                    <button onClick={() => this.props.deleteTask(this.props.task)} className="delete-button">
-                        delete
-                    </button>
+            <div className="row">
+                <div className="col-sm-12">
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="header">{this.props.task.name}</h3>
+                            <p>Category: {this.props.task.category}</p>
+                            <p>Description: {this.props.task.description}</p>
+                            <p>Duration: {this.props.task.duration}</p>
+                            <p>Has Volunteer: {this.props.task.has_volunteer ? "True" : "False"}</p>
+                            <button className="btn btn-outline-warning btn-sm" onClick={this.showEditForm}> {this.state.buttonEditLabel}</button>
+                                {
+                                    this.state.showEditForm 
+                                    ? <EditTaskForm task={this.props.task} 
+                                                    options={this.props.categories} 
+                                                    updateTask={this.props.updateTask}
+                                        /> 
+                                    : null
+                                }
+                            {/* <button><Link to='/tasks/edit'>Edit Task</Link></button>     */}
+                            <button className="btn btn-outline-danger btn-sm" onClick={() => this.props.deleteTask(this.props.task)} className="delete-button">
+                                delete
+                            </button>
+                        </div>
+                    </div>
+                
+                        
                 </div>
             </div>
+    
+            
         )
     }
     
